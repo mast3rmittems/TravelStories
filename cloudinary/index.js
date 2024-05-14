7dc994421854c2049d98ade0f88fc2b26b2d0859
mast3rmittems <97486970+mast3rmittems@users.noreply.github.com>
2024-05-14 16:20:02 -0400

    Add files via upload

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -2,7 +2,7 @@ const cloudinary = require('cloudinary').v2;
 const {CloudinaryStorage} = require('multer-storage-cloudinary');
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    cloud_name: process.env.CLOUDINARY_NAME,
     api_key: process.env.ClOUDINARY_KEY,
     api_secret: process.env.CLOUDINARY_SECRET
 });
